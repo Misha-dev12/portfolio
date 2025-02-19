3e1fcfc7a41cf7fd1af04f6bde240a7499f40816
Kolesnyk Mykhailo <devhomeworks@gmail.com>
2025-02-19 20:02:23 +0200

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,19 @@
 function App() {
-	
-	
-
-	
-
 	// scroll header
 	const headerFixed = document.querySelector('.header');
-
 	const setFixedHeader = () => {
 		headerFixed.classList.toggle('scroll-active', window.scrollY >= 60);
 	};
-
 	window.addEventListener('scroll', setFixedHeader);
 
-	/// header burger
+	// scroll back top
+	document.querySelector('.footer__back').addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+		});
+	});
 
+	/// header burger
 	const burger = document.querySelector('.burger');
 	const header = document.querySelector('.header');
 	const navItems = document.querySelectorAll('.nav__item');
@@ -25,7 +24,6 @@ function App() {
 		header.classList.remove('open-menu');
 		body.style.overflow = 'visible';
 	}
-
 	function toggleMenu() {
 		const isActive = burger.classList.toggle('active');
 		header.classList.toggle('open-menu', isActive);
@@ -41,6 +39,19 @@ function App() {
 			closeMenu();
 		}
 	});
-}
 
+	/// tabs portfolio
+
+	const buttons = document.querySelectorAll('.portfolio__tabs-btn');
+	const tabs = document.querySelectorAll('.portfolio__tab');
+
+	buttons.forEach(button => {
+		button.addEventListener('click', () => {
+			buttons.forEach(btn => btn.classList.remove('active'));
+			tabs.forEach(tab => tab.classList.remove('active'));
+			button.classList.add('active');
+			document.getElementById(button.dataset.tab).classList.add('active');
+		});
+	});
+}
 App();
